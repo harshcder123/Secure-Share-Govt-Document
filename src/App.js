d4b53e56604f8a6abfef9376b6851b21cf230004
harshcder123 <153726064+harshcder123@users.noreply.github.com>
2025-06-10 15:49:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Components
+import Navbar from "./Components/Navbar";
+ 
+import Home from "./Components/Home";
+import Register from "./Components/Register";
+import Login from "./Components/Login";
+import Deashboard from "./Components/Deashboard";
+import Uploaddoc from "./Components/Uploaddoc";
+import Footer from "./Components/Footer";
+import  Sharedoc  from "./Components/Sharedoc"; 
+import  Profile  from "./Components/Profile"; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="d-flex flex-column min-vh-100">
+        <Navbar />
+
+        <div className="flex-grow-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Deashboard />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/uploaddoc" element={<Uploaddoc />} />
+            <Route path="/sharedoc" element={<Sharedoc />} />
+            <Route path="/profile" element={<Profile />} />
+            
+          </Routes>
+        </div>
+
+        <Footer /> 
+      </div>
+    </Router>
   );
 }
 
+
+
 export default App;
+
+
+
